0d6f101e82544cdeb1843cfae75b3155826b191c
Purnendu Mishra <purnendumishra129th@gmail.com>
2022-07-04 03:29:49 +0530

    Class Based Component Are Used

--- src/Component/NewsItem.js
+++ src/Component/NewsItem.js
@@ -16,7 +16,7 @@ export class NewsItem extends Component {
           <img
             src={
               !imageUrl
-                ? "https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q"
+                ?"https://www.imagesource.com/wp-content/uploads/2019/06/Rio.jpg"
                 : imageUrl
             }
             className="card-img-top"
